035b401bf69917c481e2110abaca2f2e46bb11fa
Gr44n91 <gr44n91@gmail.com>
2023-03-03 22:52:33 +0100

    Initial commit

---
+++ src/test/java/com/openbootcampsecurity/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openbootcampsecurity.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
